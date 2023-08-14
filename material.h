b471d403e52d4bfa4dd58df157f886d4b4e88ea2
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-08-13 20:54:47 -1000

    B2S9 complete.

--- material.h
+++ material.h
@@ -125,3 +125,21 @@ public:
         return emit->value(u, v, p);
     }
 };
+
+class isotropic final : public material {
+public:
+    shared_ptr<texture> albedo;
+
+    explicit isotropic(color c) noexcept: albedo{make_shared<solid_color>(c)} {}
+    explicit isotropic(shared_ptr<texture> albedo): albedo{albedo} {}
+
+    [[nodiscard]] bool scatter(
+            const ray &r_in,
+            const hit_record &rec,
+            color &attenuation,
+            ray &scattered) const noexcept override {
+        scattered = ray(rec.p, random_in_unit_sphere(), r_in.time());
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
+        return true;
+    }
+};
